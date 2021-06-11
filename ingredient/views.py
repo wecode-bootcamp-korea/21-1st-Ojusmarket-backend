4ceea1b3b033448f49871745cfd861e88bc51fb7
박준영 <sjwm98@gmail.com>
2021-06-11 21:21:17 +0900

    CREATE:mainpage구현

--- ingredient/views.py
+++ ingredient/views.py
@@ -3,36 +3,37 @@ import json
 from django.views import View
 from django.http import JsonResponse
 
-from ingredient.models import Ingredient, IngredientSubCategory
+from ingredient.models import Ingredient, IngredientMainCategory
 from recipe.models import Recipe
 
 class MaincategoryView(View):
     def get(self, request):
 
-        ingredient_result = Ingredient.objects.all()
+        ingredient_results = Ingredient.objects.all()
 
-        ingredients_result = []
+    
+        result = []
 
-        for ingredient_results in ingredient_result:
-            ingredients_result.append({
-                'id'          : ingredient_results.id,
-                'name'        : ingredient_results.name,
-                'price'       : ingredient_results.price,
-                'storage'     : ingredient_results.storage,
-                'category_id' : ingredient_results.category_id,
-                'image_url'   : ingredient_results.image_url,
+        for ingredient_result in ingredient_results:
+            result.append({
+                'id'          : ingredient_result.id,
+                'name'        : ingredient_result.name,
+                'price'       : ingredient_result.price,
+                'storage'     : ingredient_result.storage,
+                'category_id' : ingredient_result.category.main_category_id,
+                'image_url'   : ingredient_result.image_url
             })
 
-        recipe_result = Recipe.objects.all()
+        recipe_results = Recipe.objects.all()
 
         recipes_result = []
 
-        for recipe_results in recipe_result:
+        for recipe_result in recipe_results:
             recipes_result.append({
-                'id'          : recipe_results.id,
-                'name'        : recipe_results.name,
-                'image_url'   : recipe_results.image_url,
-                'category_id' : recipe_results.category_id
+                'id'          : recipe_result.id,
+                'name'        : recipe_result.name,
+                'image_url'   : recipe_result.image_url,
+                'category_id' : recipe_result.category_id
             })
 
-        return JsonResponse({'ingredient':ingredients_result, 'recipe': recipes_result}, status=200)
+        return JsonResponse({'ingredient':result, 'recipe': recipes_result}, status=200)
