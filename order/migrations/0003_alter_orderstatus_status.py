920f6df2c0bced30e8f4b246a18194719364393a
Kevin <nellholic@gmail.com>
2021-06-15 17:19:29 +0900

    ALTER : sync name of migration file

--- order/migrations/0003_alter_orderstatus_status.py
+++ order/migrations/0003_alter_orderstatus_status.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('order', '0002_auto_20210614_0714'),
+        ('order', '0002_auto_20210614_0715'),
     ]
 
     operations = [
