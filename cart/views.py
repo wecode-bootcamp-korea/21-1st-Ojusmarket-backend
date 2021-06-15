da0a4544fd0b124783ac96db53391f32822dd8e4
ttakku-park <ttakku.park@gmail.com>
2021-06-15 17:32:15 +0900

    ADD : 장바구니 view 작성 - post : 데코레이터 삽입, 최소수량 확인 로직 작성, 중복 제품은 수량을 count up 하는 로직작성, 신규 장바구니 추가제품 db 생성 로직 작성 - get : jwt로 확인 된 유저의 장바구니 리스트 조

--- cart/views.py
+++ cart/views.py
@@ -1,56 +1,71 @@
-import json
-import bcrypt
+import json, bcrypt
 
 from django.views     import View
 from django.http      import JsonResponse
 
-from .models   import Cart
-from .models   import Ingredient
-from .models   import User 
-from user      import login_decorator
+from .models   import Cart, Ingredient, User
+
+from utils import login_decorator
 
 class CartListView(View):
     @login_decorator
     def post(self,request):
         try:
             data = json.loads(request.body)
+            user = request.user
 
             if data['count'] <= 0:
                 return JsonResponse({"message": "LESS_THAN_MINIMUM_QUANTITY"}, status=400)
 
+            duplicate_user = Cart.objects.filter(user_id=user.id)
+
+            for cart in duplicate_user :
+                if cart.ingredient_id == data['ingredient_id'] :
+                    cart.count += data['count']
+                    cart.save()
+                    return JsonResponse({'message':'ADDITIONAL_SUCCESS'}, status=200)
+                    
             Cart.objects.create(
-                count        =data['count'],
-                ingredient_id=data['ingredient_id'],
-                user_id      =data['user_id']
+                count         = data['count'],
+                ingredient_id = data['ingredient_id'],
+                user          = user
                 )
             return JsonResponse({'message':'SUCCESS'}, status=201)
-   
+        
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
-
-
+        
+        except Cart.DoesNotExist:
+          return JsonResponse({'message': 'THIS ACCOUNT DOES NOT EXIST'}, status=400)
+    
     @login_decorator
     def get(self,request):
         try:
-            users = login_decorator.user
+            user = request.user
             
-            user_id_to_ingeridient = Cart.objects.filter(user_id=users) 
-            #데코레이터로 확인한 유저의 장바구니 품목 조회 (Cart queryset)
-            
+            users_ingredient_list = Cart.objects.filter(user_id=user)
+            cart_list = []
 
-            #for ingredients in carts :
+            for cart in users_ingredient_list :
+                cart_info = {
+                    'id'       : cart.ingredient.id,
+                    'name'     : cart.ingredient.name,
+                    'price'    : cart.ingredient.price,
+                    'image_url': cart.ingredient.image_url,
+                    'count'    : cart.count
+                }
+                cart_list.append(cart_info)
 
-
+            return JsonResponse({'cart_list':cart_list}, status=200)
 
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
 
-    
+        except Cart.DoesNotExist:
+          return JsonResponse({'message': 'THIS ACCOUNT DOES NOT EXIST'}, status=400)
+
     # def delete(self,request):
     #      try:
     #          data = json.loads(request.body)
-
-
-
     #      except KeyError:
     #          return JsonResponse({"message": "KEY_ERROR"}, status=400)
