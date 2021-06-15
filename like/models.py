aadd63591d25668afa8ba980d906061a24610b80
박준영 <sjwm98@gmail.com>
2021-06-15 17:50:33 +0900

    FIX: 수정완료

--- like/models.py
+++ like/models.py
@@ -7,12 +7,7 @@ class Like(models.Model):
     created_at   = models.DateTimeField(auto_now_add=True)
     user         = models.ForeignKey(User, on_delete=models.CASCADE,related_name='like_user')
     ingredient   = models.ForeignKey(Ingredient, on_delete=models.CASCADE,related_name='like_ingredient')
-<<<<<<< HEAD
     false_delete = models.BooleanField(default=False)
 
-=======
-    false_delete = models.BooleanField(default=False) 
-    
->>>>>>> main
     class Meta:
         db_table = 'likes'
