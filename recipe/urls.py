1008d8ff273cd64ad91360afbd72e862c9a8be28
박준영 <sjwm98@gmail.com>
2021-06-16 16:21:03 +0900

    ADD:ingredient 수정

--- recipe/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.views.generic.base import View
-from .views import RecipesView
-
-urlpatterns = [
-    path('',RecipesView.as_view()),
-    path('/recipe',RecipesView.as_view())
-]
