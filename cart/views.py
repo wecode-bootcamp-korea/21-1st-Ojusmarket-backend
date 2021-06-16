2166ce2f149fa1527604224f37085b1ea283f8e2
ttakku-park <ttakku.park@gmail.com>
2021-06-17 03:08:23 +0900

    FIX : 컨벤션 수정

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-import json, bcrypt
+import json
 
 from django.views     import View
 from django.http      import JsonResponse
@@ -8,6 +8,7 @@ from .models   import Cart
 from utils import login_decorator
 
 class CartListView(View):
+
     @login_decorator
     def post(self,request):
         try:
@@ -24,17 +25,17 @@ class CartListView(View):
                 return JsonResponse({'message':'ADDITIONAL_SUCCESS'}, status=200) 
                     
             Cart.objects.create(
-                    count         = data['count'],
-                    ingredient_id = data['ingredient_id'],
-                    user          = user
-                    )
+                count         = data['count'],
+                ingredient_id = data['ingredient_id'],
+                user          = user
+                )
             return JsonResponse({'message':'SUCCESS'}, status=201)
         
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
         
         except Cart.DoesNotExist:
-          return JsonResponse({'message': 'THIS_ACCOUNT_DOES_NOT_EXIST'}, status=400)
+            return JsonResponse({'message': 'THIS_ACCOUNT_DOES_NOT_EXIST'}, status=400)
     
     @login_decorator
     def get(self,request):
@@ -42,23 +43,22 @@ class CartListView(View):
             users_ingredient_list = Cart.objects.filter(user=request.user)
             cart_list = []
 
-            for cart in users_ingredient_list :
+            for cart in users_ingredient_list:
                 cart_info = {
                     'id'       : cart.ingredient.id,
                     'name'     : cart.ingredient.name,
                     'price'    : cart.ingredient.price,
                     'image_url': cart.ingredient.image_url,
                     'count'    : cart.count
-                }
+                    }
                 cart_list.append(cart_info)
-
             return JsonResponse({'cart_list':cart_list}, status=200)
 
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
 
         except Cart.DoesNotExist:
-          return JsonResponse({'message': 'THIS_ACCOUNT_DOES_NOT_EXIST'}, status=400)
+            return JsonResponse({'message': 'THIS_ACCOUNT_DOES_NOT_EXIST'}, status=400)
 
     @login_decorator
     def delete(self,request):
@@ -66,21 +66,21 @@ class CartListView(View):
             data = json.loads(request.body)
             user = request.user
             
-            [Cart.objects.filter(user_id=user.id, ingredient_id = data_i['ingredient_id']).delete() for data_i in data]
+            [Cart.objects.filter(user_id=user.id, ingredient_id=data_i['ingredient_id']).delete() for data_i in data]
             return JsonResponse({"message": "SUCCESSFUL_DELETION"}, status=200)
 
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
         
         except Cart.DoesNotExist:
-          return JsonResponse({'message': 'THIS_ACCOUNT_DOES_NOT_EXIST'}, status=400)
+            return JsonResponse({'message': 'THIS_ACCOUNT_DOES_NOT_EXIST'}, status=400)
 
     @login_decorator
     def patch(self,request):
         try:
             data = json.loads(request.body)
             
-            for data_i in data :
+            for data_i in data:
                 cart = Cart.objects.get(user=request.user, ingredient_id=data_i['ingredient_id'])
                 Cart.objects.filter(user=request.user, ingredient_id=data_i['ingredient_id'], count=cart.count).update(count=data_i['count'])
             return JsonResponse({"message": "SUCCESSFUL_UPDATE"}, status=200)
@@ -89,4 +89,4 @@ class CartListView(View):
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
         
         except Cart.DoesNotExist:
-          return JsonResponse({'message': 'THIS_ACCOUNT_DOES_NOT_EXIST'}, status=400)
+            return JsonResponse({'message': 'THIS_ACCOUNT_DOES_NOT_EXIST'}, status=400)
