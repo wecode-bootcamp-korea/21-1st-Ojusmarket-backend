3bff4457a6393a998ab1ce776f2be982c563081b
박준영 <sjwm98@gmail.com>
2021-06-16 10:32:20 +0900

    FIX:주석제거

--- recipe/views.py
+++ recipe/views.py
@@ -9,18 +9,11 @@ class RecipesView(View):
     def get(self, request):
         category_id = request.GET.get('category_id', None)
 
-        # CASE 1
         q = Q()
         if category_id:
             q &= Q(category_id=category_id)
         recipes = Recipe.objects.filter(q)
-
-        # CASE 2
-        # recipes = Recipe.objects.all()
-
-        # if category_id:
-        #     recipes = recipes.filter(category_id=category_id)
-
+        
         results = []
 
         for recipe in recipes:
