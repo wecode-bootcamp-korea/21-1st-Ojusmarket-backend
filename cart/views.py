9d05e62e01e3e721bb909fbccaa54f5fe1f7a059
ttakku-park <ttakku.park@gmail.com>
2021-06-17 15:42:12 +0900

    FIX : cart views.py 로직 수정 및 컨벤션 수정

--- cart/views.py
+++ cart/views.py
@@ -3,9 +3,8 @@ import json
 from django.views     import View
 from django.http      import JsonResponse
 
-from .models   import Cart
-
-from utils import login_decorator
+from .models import Cart
+from utils   import login_decorator
 
 class CartListView(View):
 
@@ -18,18 +17,18 @@ class CartListView(View):
             if data['count'] <= 0:
                 return JsonResponse({"message": "LESS_THAN_MINIMUM_QUANTITY"}, status=400) 
 
-            if Cart.objects.filter(user=user, ingredient_id=data['ingredient_id']).exists():
-                cart = Cart.objects.get(user=user, ingredient_id=data['ingredient_id'])
+            if Cart.objects.filter(user=user, ingredient_id=data['id']).exists():
+                cart = Cart.objects.get(user=user, ingredient_id=data['id'])
                 cart.count += data['count']
                 cart.save()
                 return JsonResponse({'message':'ADDITIONAL_SUCCESS'}, status=200) 
                     
             Cart.objects.create(
                 count         = data['count'],
-                ingredient_id = data['ingredient_id'],
+                ingredient_id = data['id'],
                 user          = user
                 )
-            return JsonResponse({'message':'SUCCESS'}, status=201)
+            return JsonResponse({'message':'SUCCESS'}, status=200)
         
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
@@ -40,18 +39,17 @@ class CartListView(View):
     @login_decorator
     def get(self,request):
         try:
-            users_ingredient_list = Cart.objects.filter(user=request.user,soft_delete=False)
+            carts = Cart.objects.filter(user=request.user,soft_delete=False)
             cart_list = []
 
-            for cart in users_ingredient_list:
-                cart_info = {
+            for cart in carts:
+                cart_list.append({
                     'id'       : cart.ingredient.id,
                     'name'     : cart.ingredient.name,
                     'price'    : cart.ingredient.price,
                     'image_url': cart.ingredient.image_url,
                     'count'    : cart.count
-                    }
-                cart_list.append(cart_info)
+                    })    
             return JsonResponse({'cart_list':cart_list}, status=200)
 
         except KeyError:
@@ -65,9 +63,12 @@ class CartListView(View):
         try:
             data = json.loads(request.body)
             user = request.user
-            
-            [Cart.objects.filter(user_id=user.id, ingredient_id=data_i['ingredient_id']).delete() for data_i in data]
-            return JsonResponse({"message": "SUCCESSFUL_DELETION"}, status=200)
+            data_list = []
+
+            [data_list.append(cart['id']) for cart in data]
+            carts = Cart.objects.filter(user_id=user.id, ingredient_id__in=data_list)
+            carts.delete()
+            return JsonResponse({"message": "SUCCESSFUL_DELETION"}, status=204)
 
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
@@ -81,8 +82,8 @@ class CartListView(View):
             data = json.loads(request.body)
             
             for data_i in data:
-                cart = Cart.objects.get(user=request.user, ingredient_id=data_i['ingredient_id'])
-                Cart.objects.filter(user=request.user, ingredient_id=data_i['ingredient_id'], count=cart.count).update(count=data_i['count'])
+                cart = Cart.objects.get(user=request.user, ingredient_id=data_i['id'])
+                Cart.objects.filter(user=request.user, ingredient_id=data_i['id'], count=cart.count).update(count=data_i['count'])
             return JsonResponse({"message": "SUCCESSFUL_UPDATE"}, status=200)
 
         except KeyError:
