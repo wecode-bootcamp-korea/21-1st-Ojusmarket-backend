c2d9aec32a0928c17d93d099c1e9d8f9c79e4942
박준영 <sjwm98@gmail.com>
2021-06-20 17:33:13 +0900

    FIX:cart models 수정

--- cart/models.py
+++ cart/models.py
@@ -8,7 +8,6 @@ class Cart(models.Model):
     count       = models.IntegerField()
     user        = models.ForeignKey(User, on_delete=models.CASCADE)
     ingredient  = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    soft_delete = models.BooleanField(default=False)
     
     class Meta:
         db_table = 'carts'
